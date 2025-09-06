15c8d67283715ba2dcd0ed954c0ff864f5686854
Agustina Armendariz <armendarizagustina.m@gmail.com>
2025-09-06 17:39:52 -0300

    bg fixed

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -7,16 +7,18 @@ import {images} from "@/utils/images";
 import Aurora from "./Aurora";
 export default function Landing() {
     return (
-        <div className="w-full h-max-screen flex flex-col items-center justify-center">
-          
-          <Aurora
-        colorStops={["#743596","#AC80C6","#AAB1BF" ]}
-        blend={0.5}
-        amplitude={3.0}
-        speed={0.5}
-      />
-      
-       <h1 className="text-white text-6xl font-semibold italic font-[lora] mb-10 mt-0">Insspira</h1>
+        <div className="relative w-full min-h-screen flex flex-col items-center justify-center overflow-hidden">
+     <div className="absolute inset-0 z-0 pointer-events-none">
+        <Aurora
+          colorStops={["#743596","#AC80C6","#AAB1BF"]}
+          blend={0.5}
+          amplitude={2.0}
+          speed={0.5}
+        />
+      </div>
+      <div className="relative z-10 w-full max-w-6xl px-4">
+       <h1 className="text-white text-center text-6xl font-semibold italic font-[lora] mt-10 mb-10">Insspira</h1>
+       
         <Swiper
         modules={[EffectCoverflow, Pagination, Autoplay]}
         effect="coverflow"
@@ -28,6 +30,7 @@ export default function Landing() {
           rotate: 45,
           stretch: 0,
           depth: 200,
+          scale: 1,
           modifier: 1,
           slideShadows: true,
         }}
@@ -39,9 +42,9 @@ export default function Landing() {
         {images.map((src, index) => (
           <SwiperSlide
             key={index}
-            className="w-[200px] md:w-[300px] h-full"
+            className="w-[200px] md:w-[300px] h-full rounded-2xl overflow-hidden shadow-lg"
           >
-            <div className="relative w-full h-full">
+            <div className="relative w-full h-full rounded-2xl overflow-hidden">
               <Image
                 src={src}
                 alt={`slide-${index}`}
@@ -52,12 +55,14 @@ export default function Landing() {
           </SwiperSlide>
         ))}
       </Swiper>
-      <h3 className="text-white text-lg font-[lora] mt-4 mb-4">Share your world, inspire others</h3>
-      <div className="flex items-center justify-center mb-2 mt-4">
-        <button className="text-white font-bold py-2 px-4 rounded-full bg-[var(--morado)] hover:bg-[var(--rosa)] transition-colors">
+      </div>
+      <h3 className="text-white text-xl font-[montserrat] mt-4 mb-4">Share your world, inspire others</h3>
+      <div className="flex items-center justify-center mb-2 ">
+        <button className="text-white font-bold py-4 px-6 rounded-full bg-[var(--morado)] hover:bg-[var(--rosa)] transition-colors">
           Start explore
         </button>
       </div>
+       
     </div>
   
 
