3b5365af4add6dedfbc9dab3bfb4f08c7d97297a
NicolasDelFabro <nicolasdelfabro123@gmail.com>
2025-09-18 20:15:54 -0300

    Pin modal & Searche, build

--- src/components/pins/PinsList.tsx
+++ src/components/pins/PinsList.tsx
@@ -1,65 +1,3 @@
-// 'use client'
-
-// import PinsCard from "@/components/pins/PinsCard";
-// import { useEffect, useState } from "react";
-// import type { IPins } from "@/interfaces/IPins";
-// import { getAllPins } from "@/services/pins.services";
-// import PinModal from "./PinModal";
-// interface PinsListProps {
-//   searchResults: IPins[] | null;
-// }
-
-// const PinsList: React.FC<PinsListProps> = ({ searchResults }) => {
-//   const [allPins, setAllPins] = useState<IPins[]>([]);
-//   const [isOpen, setIsOpen] = useState(false);
-//   const [pinSelected, setPinSelected] = useState<string | null>(null);
-
-//   useEffect(() => {
-//     if (searchResults === null) {
-//       const fetchPins = async () => {
-//         const data = await getAllPins();
-//         setAllPins(data || []);
-//       };
-//       fetchPins();
-//     }
-//   }, [searchResults]);
-
-//   const displayedPins = searchResults !== null ? searchResults : allPins;
-
-//   const normalizePin = (pin: IPins): IPins => ({
-//     ...pin,
-//     image: pin.image?.trim() ? pin.image : "/architecture.jpg",
-//   });
-
-//   return (
-//       <div className="flex justify-center h-auto px-4 bg-gradient-to-r from-[#0E172B] to-[#1B273B]">
-//       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-8 lg:gap-10">
-//         {displayedPins.map(pin => (
-//           <div key={pin.id} onClick={() => {
-//             setPinSelected(pin.id);
-//             setIsOpen(true);
-//           }}
-//           >
-//             <PinsCard pin={normalizePin(pin)} />
-//           </div>
-//         ))}
-//         {displayedPins.length === 0 && <p>No se encontraron resultados</p>}
-//       </div>
-//       {isOpen && pinSelected && (
-//   <PinModal
-//     id={pinSelected}
-//     onClose={() => {
-//       setIsOpen(false);
-//       setPinSelected(null);
-//     }}
-//   />
-// )}
-//     </div>
-//   );
-// };
-
-// export default PinsList;
-
 'use client'
 
 import PinsCard from "@/components/pins/PinsCard";
@@ -70,6 +8,18 @@ import { getAllPins } from "@/services/pins.services";
 
 interface PinsListProps {
   searchResults: IPins[] | null;
+}
+
+interface PinsList {
+  id: string;
+  image?: string | null;
+  description?: string | null;
+  likesCount: number;
+  commentsCount: number;
+  views: number;
+  user: string;
+  likes?: number;
+  comment?: number;
 }
 
 const PinsList: React.FC<PinsListProps> = ({ searchResults }) => {
--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -1,30 +1,3 @@
-// 'use client'
-
-// import { useEffect } from "react";
-// import PinsList from "@/components/pins/PinsList";
-// import { usePinsSearch } from "@/hooks/pins/usePinsSearch";
-// import { saveTokenFromQueryAndHydrateAuth } from "@/services/authservice";
-// import { useAuth } from "@/context/AuthContext";
-
-// const Home = () => {
-//   const { results } = usePinsSearch();
-//   const { setAuth } = useAuth();
-
-//   useEffect(() => {
-//     // Si vienes de Auth0 con ?token=..., lo guardamos y actualizamos el AuthContext
-//     saveTokenFromQueryAndHydrateAuth(setAuth);
-//   }, [setAuth]);
-
-//   return (
-//     <div>
-//       <PinsList searchResults={results} />
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-// pages/home.tsx
 'use client'
 
 import PinsList from "@/components/pins/PinsList";
