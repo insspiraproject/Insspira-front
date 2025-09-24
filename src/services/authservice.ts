f29f4225dd9518ac2e0c869beeaeb46995654d24
NicolasDelFabro <nicolasdelfabro123@gmail.com>
2025-09-23 22:03:58 -0300

    variable de entorno

--- src/services/authservice.ts
+++ src/services/authservice.ts
@@ -22,7 +22,7 @@ export interface LoginResponse {
 export type RegisterResponse = LoginResponse;
 
 const API_BASE =
-  process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/+$/, "") ?? "http://localhost:3000";
+  process.env.NEXT_PUBLIC_API_URL?.replace(/\/+$/, "") ?? "http://localhost:3000";
 
 /* ================= helpers ================= */
 
